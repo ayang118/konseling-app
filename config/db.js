217288ee02ff98a4102cc4653a7c54daa12821dd
ayang118 <riskamby@gmail.com>
2026-01-07 19:28:03 +0700

    fix db connection for railway mysql

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "konseling"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
